5318e30d64be2cbf0acc4f3fbe3b4c381a38d7a3
odayabueid <odayabueid1992m@gmail.com>
2019-05-17 23:29:55 +0300

    test solution

--- server/Controllers/chefControllers.js
+++ server/Controllers/chefControllers.js
@@ -1,26 +1,68 @@
-const chef = require("../db/db.js");
+const db = require("../db/db.js");
 
 // creates new chef
-exports.createChef = function(req, res) {};
+exports.createChef = function(req, res) {
+	db.Chef.create({
+		username:req.body.username,
+		password:req.body.password,
+		location:req.body.location,
+		phoneNumber:req.body.phoneNumber,
+		description:req.body.description,
+		rating:req.body.rating,
+		imgUrl:req.body.imgUrl
+	}).then(chef =>{
+		res.send(chef)
+	})
+};
 
 // gets the data for all the chefs
-exports.retrieveAllChefs = function(req, res) {};
+exports.retrieveAllChefs = function(req, res) {
+	db.Chef.findAll().then(chef =>{
+		res.send(chef)
+	})
+};
 
 // gets data for one chef
-exports.retrieveOneChef = function(req, res) {};
+exports.retrieveOneChef = function(req, res) {
+	
+};
 
 // gets the chefs with specific location
 exports.retrieveByLocation = function(req, res) {};
 
 // updates Information for the cheif
-exports.updateOne = function(req, res) {};
+exports.updateOne = function(req, res) {
+	db.Chef.update({
+		username:req.body.username,
+		password:req.body.password,
+		location:req.body.location,
+		phoneNumber:req.body.phoneNumber,
+		description:req.body.description,
+		rating:req.body.rating,
+		imgUrl:req.body.imgUrl},{where:{username:req.params.username}}
+		).then(()=>{
+			res.send("updated successfully for this chef")
+		})
+};
 
 ///////////////////////////////////////////////////
 //mealsControllers
 
 exports.createMeal = function(req, res) {};
-exports.deleteMeal = function(req, res) {};
+exports.deleteMeal = function(req, res) {
+	db.Meal.destroy({
+		where:{name:req.params.username}
+	}).then(()=>{
+		res.send('deleted meal successfully')
+	})
+};
 
 // if the chef decides to delete his account
 // for extra time
-exports.deleteOneChef = function(req, res) {};
+exports.deleteOneChef = function(req, res) {
+	db.Chef.destroy({
+		where:{username:req.params.username}
+	}).then(()=>{
+		res.send('deleted chef successfully')
+	})
+};
