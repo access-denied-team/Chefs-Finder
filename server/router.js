b06193b08c27dff0349cdb1f3618de4e24d72129
Yasser <eng.yasserkassem@gmail.com>
2019-05-17 18:26:47 +0300

    Routes Finshed

--- server/router.js
+++ server/router.js
@@ -1,10 +1,17 @@
 const router= require("express").Router()
 const chefControllers = require("./Controllers/chefControllers")
+const Chef= require("./db/db")
 
 //routes 
 router.route("/signup").post((req,res)=>{
     chefControllers.createChef(req,res)
 })
+
+
+router.route("/login").post((req,res)=>{
+    
+})
+
 
 router.route("/all").get((req,res)=>{
     chefControllers.retrieveAllChefs(req,res)
@@ -24,6 +31,16 @@ router.route("/:username")
 router.route("/:location").get((req,res) => {
     chefControllers.retrieveByLocation(req,res)
 })
+
+// meal routes
+router.route("/:username/meal").post((req,res)=>{
+    chefControllers.createMeal(req,res)
+}).delete((req,res)=>{
+    chefControllers.deleteMeal(req,res)
+})
+
+
+
 
 
 
