6d33bc7a7a6f9b8b59b55b4d70b70ccb63c49293
Shareef <shareefnuseibeh@yahoo.com>
2019-05-20 21:36:28 +0300

    unknown changes

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 3030;
 
 ////////////////////////////////////////////////////////
-app.use(express.static("../frontend"));
+app.use(express.static("frontend"));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
